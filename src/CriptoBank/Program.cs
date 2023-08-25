e361205053db39e0e2430f8ecd81fc20c53f4223
iztleu <diztleu@1cb.kz>
2023-08-25 15:04:03 +0600

    some change

--- src/CriptoBank/Program.cs
+++ src/CriptoBank/Program.cs
@@ -1,5 +1,47 @@
 ﻿// See https://aka.ms/new-console-template for more information
+unsafe
+{
+    Point point = new Point(0, 0);
+    point.Value = new("Point1", 1);
+    Console.WriteLine(point);   // X: 0  Y: 0
+    Point* p = &point;
+ 
+    p->X = 30;
+    Console.WriteLine(p->X);    // 30
 
-double a = -0;
-double b = 1 / a;
-Console.WriteLine(b);
+    p->Value = new("Point2", 2);
+    Console.WriteLine(p->Value.ToString());
+ 
+    // разыменовывание указателя
+    (*p).Y = 180;
+    Console.WriteLine((*p).Y);  // 180
+ 
+    Console.WriteLine(point);   // X: 30  Y: 180
+}
+
+
+struct Point
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+    public Value Value { get; set; }
+    public Point(int x, int y)
+    {
+        X = x; Y = y;
+    }
+    public override string ToString() => $"X: {X}  Y: {Y}";
+}
+
+class Value
+{
+    string Name;
+    int Age;
+
+    public Value(string name, int age)
+    {
+        Name = name;
+        Age = age;
+    }
+    
+    public override string ToString() => $"Name: {Name}  Age: {Age}";
+}
