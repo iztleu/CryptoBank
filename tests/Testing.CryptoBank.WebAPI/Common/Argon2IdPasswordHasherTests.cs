a68f2a641e3c72b85b0e526ddd4b1fe74466845c
iztleu <diztleu@1cb.kz>
2023-09-05 19:18:30 +0600

    added tests

--- tests/Testing.CryptoBank.WebAPI/Common/Argon2IdPasswordHasherTests.cs
+++ tests/Testing.CryptoBank.WebAPI/Common/Argon2IdPasswordHasherTests.cs
@@ -49,4 +49,38 @@ public class Argon2IdPasswordHasherTests
         var expectedHashBytes = argon2Id.GetBytes(options.PasswordHashSizeInBytes);
         hashBytes.Should().BeEquivalentTo(expectedHashBytes);
     }
+    
+    [Fact]
+    public void VerifyHashedPassword_should_return_true_for_correct_hash()
+    {
+        // Arrange
+        var hash = "argon2id$m=4096$i=3$p=4$HKPdgfRQ14YaszwdAWFpew==$mgzo7Z33PQbH+oyJwJiYCD9tMYxL+z1FCsLFx99ZNvY=";
+
+        var hasher = new Argon2PasswordHasher(Options.Create<Argon2ConfigOptions>(new()));
+
+        // Act
+        var result = hasher.Verify(hash, "test_password");
+
+        // Assert
+        result.Should().BeTrue();
+    }
+    
+    [Theory]
+    // Changing each parameter should result in a different hash
+    [InlineData("argon2id$m=5096$i=3$p=4$HKPdgfRQ14YaszwdAWFpew==$mgzo7Z33PQbH+oyJwJiYCD9tMYxL+z1FCsLFx99ZNvY=")]
+    [InlineData("argon2id$m=4096$i=4$p=4$HKPdgfRQ14YaszwdAWFpew==$mgzo7Z33PQbH+oyJwJiYCD9tMYxL+z1FCsLFx99ZNvY=")]
+    [InlineData("argon2id$m=4096$i=3$p=5$HKPdgfRQ14YaszwdAWFpew==$mgzo7Z33PQbH+oyJwJiYCD9tMYxL+z1FCsLFx99ZNvY=")]
+    [InlineData("argon2id$m=4096$i=3$p=4$IKPdgfRQ14YaszwdAWFpew==$mgzo7Z33PQbH+oyJwJiYCD9tMYxL+z1FCsLFx99ZNvY=")]
+    [InlineData("argon2id$m=4096$i=3$p=4$HKPdgfRQ14YaszwdAWFpew==$ngzo7Z33PQbH+oyJwJiYCD9tMYxL+z1FCsLFx99ZNvY=")]
+    public void VerifyHashedPassword_should_return_false_if_parameters_or_salt_or_hash_modified(string hash)
+    {
+        // Arrange
+        var hasher = new Argon2PasswordHasher(Options.Create<Argon2ConfigOptions>(new()));
+
+        // Act
+        var result = hasher.Verify(hash, "test_password");
+
+        // Assert
+        result.Should().BeFalse();
+    }
 }
--- tests/Testing.CryptoBank.WebAPI/UnitTest1.cs
+++
@@ -1,9 +0,0 @@
-namespace Testing.CryptoBank.WebAPI;
-
-public class UnitTest1
-{
-    [Fact]
-    public void Test1()
-    {
-    }
-}
